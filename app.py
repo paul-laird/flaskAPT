213c589e55fd03d867c570c74506d086e7a246b6
paul-laird <35932444+paul-laird@users.noreply.github.com>
2022-12-02 12:22:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ mysql.init_app(app)
 @app.route("/add", methods=['GET', 'POST']) #Add Student
 def add():
   if request.method == 'POST':
-    name = request.args.post('name')
-    email = request.args.post('email')
+    name = request.data['name']
+    email = request.data['email']
     cur = mysql.connection.cursor() #create a connection to the SQL instance
     s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email)
     cur.execute(s)

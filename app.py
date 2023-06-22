3fb9458f71ca7aa2f3241be99d8d292b3f04a5aa
paul-laird <35932444+paul-laird@users.noreply.github.com>
2023-06-22 13:45:45 +0100

    change to mysql-connect-python

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask_mysqldb import MySQL
+from mysql-connect-python import mysql.connector
 from flask_cors import CORS
 import json
-mysql = MySQL()
+mysql = mysql.connector.connect(user='web', password='webPass',
+  host='127.0.0.1',
+  database='student')
 app = Flask(__name__)
 CORS(app)
 # My SQL Instance configurations
 # Change the HOST IP and Password to match your instance configurations
-app.config['MYSQL_USER'] = 'web1'
-app.config['MYSQL_PASSWORD'] = 'webPass'
-app.config['MYSQL_DB'] = 's'
-app.config['MYSQL_HOST'] = 'localhost' #for now
-mysql.init_app(app)
 
 @app.route("/add", methods=['GET', 'POST']) #Add Student
 def add():
@@ -21,17 +18,17 @@ def add():
     name = request.form['name']
     email = request.form['email']
     print(name,email)
-    cur = mysql.connection.cursor() #create a connection to the SQL instance
+    cur = mysql.cursor() #create a connection to the SQL instance
     s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email)
     cur.execute(s)
-    mysql.connection.commit()
+    mysql.commit()
   else:
     return render_template('add.html')
 
   return '{"Result":"Success"}'
 @app.route("/") #Default - Show Data
 def hello(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  cur = mysql.cursor() #create a connection to the SQL instance
   cur.execute('''SELECT * FROM students''') # execute an SQL statment
   rv = cur.fetchall() #Retreive all rows returend by the SQL statment
   Results=[]
